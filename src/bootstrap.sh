f30661982f3f7eb9a87c44923ce90a1649cbd334
Ian Page Hands <iphands@gmail.com>
2018-11-06 11:35:05 -0500

    initial

---
+++ src/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COMMON_URL="https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master/"
+curl $COMMON_URL/src/after_success.sh > .travis/after_success.sh
+curl $COMMON_URL/src/release.sh > .travis/release.sh
+curl $COMMON_URL/src/Jenkinsfile > .travis/58231b16fdee45a03a4ee3cf94a9f2c3
+
+bash .travis/after_success.sh
---
+++ src/release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+git clone ${REPO}.git -b $1
+cd dist
+
+if [[ "${TRAVIS_BRANCH}" = "prod-stable" || "${TRAVIS_BRANCH}" = "prod-beta" ]]
+then
+    cp ../.travis/Jenkinsfile ./Jenkinsfile
+fi
+
+git init
+git config --global user.name $COMMIT_AUTHOR_USERNAME
+git config --global user.email $COMMIT_AUTHOR_EMAIL
+git remote add travis-build ${REPO}.git
+git add .
+git commit -m "${TRAVIS_COMMIT_MESSAGE}"
+git push --force --set-upstream travis-build HEAD:$1
