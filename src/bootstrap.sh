7634661863348ef944e4252e71bd01e5f13dd96c
Ian Page Hands <iphands@gmail.com>
2018-11-06 12:28:39 -0500

    fix perms again O_o

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -7,4 +7,5 @@ curl $COMMON_URL/src/after_success.sh > .travis/after_success.sh
 curl $COMMON_URL/src/release.sh > .travis/release.sh
 curl $COMMON_URL/src/Jenkinsfile > .travis/58231b16fdee45a03a4ee3cf94a9f2c3
 
-bash -x .travis/after_success.sh
+chmod 755 .travis/*sh
+.travis/after_success.sh
