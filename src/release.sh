c01145f4b824cda48b34b97d40c5f8f412f90a3a
Ian Page Hands <iphands@gmail.com>
2018-11-06 13:35:03 -0500

    depend on projects package.json appname

--- src/release.sh
+++ src/release.sh
@@ -2,12 +2,15 @@
 set -e
 set -x
 
+APP_NAME=`node -e 'console.log(require("./package.json").insights.appname)'`
+
 git clone ${REPO}.git -b $1
 cd dist
 
 if [[ "${TRAVIS_BRANCH}" = "prod-stable" || "${TRAVIS_BRANCH}" = "prod-beta" ]]
 then
     cp ../.travis/58231b16fdee45a03a4ee3cf94a9f2c3 ./58231b16fdee45a03a4ee3cf94a9f2c3
+    sed -s "s/__APP_NAME__/$APP_NAME/" -i ./58231b16fdee45a03a4ee3cf94a9f2c3
 fi
 
 git init
