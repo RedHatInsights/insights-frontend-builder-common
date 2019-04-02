dcf191e2494209721b1d090a7b75eccbc243dc74
Asa Price <aprice@redhat.com>
2019-04-02 14:03:40 -0400

    Get index_map from Chrome repo

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -3,10 +3,11 @@ set -e
 set -x
 
 COMMON_URL="https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master/"
+CHROME_URL="https://raw.githubusercontent.com/RedHatInsights/insights-chrome/master/"
 curl $COMMON_URL/src/after_success.sh > .travis/after_success.sh
 curl $COMMON_URL/src/release.sh > .travis/release.sh
 curl $COMMON_URL/src/Jenkinsfile > .travis/58231b16fdee45a03a4ee3cf94a9f2c3
-curl $COMMON_URL/src/index_map.json > .travis/index_map.json
+curl $CHROME_URL/config/index_map.json > .travis/index_map.json
 
 chmod 755 .travis/*sh
 .travis/after_success.sh
