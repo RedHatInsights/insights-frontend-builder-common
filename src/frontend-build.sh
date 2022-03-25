49d03e0798aa4ea2fa6d63eb4b034a6ef8ec742d
Karel Hala <khala@redhat.com>
2022-03-25 13:09:05 +0100

    Update build container to f4bcca2 (#91)

--- src/frontend-build.sh
+++ src/frontend-build.sh
@@ -35,7 +35,7 @@ docker run -i --name $CONTAINER_NAME \
   -e IS_PR=$IS_PR \
   -e CI_ROOT=$CI_ROOT \
   -e NODE_BUILD_VERSION=$NODE_BUILD_VERSION \
-  quay.io/cloudservices/frontend-build-container:107de6b
+  quay.io/cloudservices/frontend-build-container:f4bcca2
 TEST_RESULT=$?
 
 if [ $TEST_RESULT -ne 0 ]; then
