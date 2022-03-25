0f14f487a40da7541b0bd0755596e2be795286ff
mkholjur <mkholjur@redhat.com>
2022-03-25 11:03:58 +0100

    Update after_succes scripts to remove ci and qa envs

--- src/after_success.sh
+++ src/after_success.sh
@@ -24,13 +24,13 @@ if [ -x .travis/custom_release.sh ]
 then
     .travis/custom_release.sh
 else
-    # If current dev branch is master, push to build repo ci-beta
+    # If current dev branch is master, push to build repo stage-beta
     if [[ "${TRAVIS_BRANCH}" = "master" ||  "${TRAVIS_BRANCH}" = "main" ]]; then
-        .travis/release.sh "ci-beta"
+        .travis/release.sh "stage-beta"
     fi
 
     # If current dev branch is deployment branch, push to build repo
-    if [[ "${TRAVIS_BRANCH}" = "ci-stable" || "${TRAVIS_BRANCH}" = "qa-beta" || "${TRAVIS_BRANCH}" = "qa-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" || "${TRAVIS_BRANCH}" = "qaprodauth-stable" || "${TRAVIS_BRANCH}" = "qaprodauth-beta" || "${TRAVIS_BRANCH}" = "stage-beta" || "${TRAVIS_BRANCH}" = "stage-stable" ]]; then
+    if [[ "${TRAVIS_BRANCH}" = "stage-stable" || "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" || "${TRAVIS_BRANCH}" = "qaprodauth-stable" || "${TRAVIS_BRANCH}" = "qaprodauth-beta" ]]; then
         .travis/release.sh "${TRAVIS_BRANCH}"
     fi
 fi
