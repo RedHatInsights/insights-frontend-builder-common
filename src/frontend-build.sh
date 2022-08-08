ae9c32791c72ac76b180c6d3cb79afa38b6b7754
bholifie <bholifie@redhat.com>
2022-08-08 09:17:59 -0400

    use Caddy fe build container

--- src/frontend-build.sh
+++ src/frontend-build.sh
@@ -36,7 +36,7 @@ docker run -i --name $CONTAINER_NAME \
   -e CI_ROOT=$CI_ROOT \
   -e NODE_BUILD_VERSION=$NODE_BUILD_VERSION \
   -e SERVER_NAME=$SERVER_NAME \
-  quay.io/cloudservices/frontend-build-container:d60de0c
+  quay.io/cloudservices/frontend-build-container:05ba597
 TEST_RESULT=$?
 
 if [ $TEST_RESULT -ne 0 ]; then
