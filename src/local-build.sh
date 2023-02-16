24587d76e72a78456cd0230eacd08d3b0c13b16a
Adam Drew <addrew@redhat.com>
2023-02-16 14:37:52 -0500

    Bump to new version of build container. Added local frontend test script

--- src/local-build.sh
+++ src/local-build.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+# Local build script for front ends
+# This script is used to build the front end locally
+# Use this to test the frontend build system locally when making changes
+
+# Usage:
+# 1. Clone a frontend like https://github.com/RedHatInsights/edge-frontend/
+# 2. Copy this file into the root of the repo
+# 3. Run the script
+# 4. Run the container podman run -p 8000:8000 localhost/edge:5316bd7
+# 5. Open the app in your browser at http://localhost:8000/apps/edge 
+#
+# Note: You can find the image name and tag by looking at the output of the script
+# or by running `podman images`. Also, fill in the app name in the URL with the 
+# app you are testing
+
 # --------------------------------------------
 # Export vars for helper scripts to use
 # --------------------------------------------
+export WORKSPACE=$(pwd)
 export APP_NAME=$(node -e "console.log(require(\"${WORKSPACE:-.}${APP_DIR:-}/package.json\").insights.appname)")
 export CONTAINER_NAME="$APP_NAME-build-main"
 # main IMAGE var is exported from the pr_check.sh parent file
+export IMAGE="$APP_NAME-build-main"
 export IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 export IS_PR=false
 COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master
-
-function teardown_docker() {
-  docker rm -f $CONTAINER_NAME || true
-}
-
-trap "teardown_docker" EXIT SIGINT SIGTERM
 
 # Get the chrome config from cloud-services-config
 function get_chrome_config() {
@@ -51,31 +62,12 @@ function get_chrome_config() {
   return 0
 }
 
-
-# Job name will contain pr-check or build-master. $GIT_BRANCH is not populated on a
-# manually triggered build
-if echo $JOB_NAME | grep -w "pr-check" > /dev/null; then
-  if [ ! -z "$ghprbPullId" ]; then
-    export IMAGE_TAG="pr-${ghprbPullId}-${IMAGE_TAG}"
-    CONTAINER_NAME="${APP_NAME}-pr-check-${ghprbPullId}"
-  fi
-
-  if [ ! -z "$gitlabMergeRequestIid" ]; then
-    export IMAGE_TAG="pr-${gitlabMergeRequestIid}-${IMAGE_TAG}"
-    CONTAINER_NAME="${APP_NAME}-pr-check-${gitlabMergeRequestIid}"
-  fi
-
-  IS_PR=true
-fi
-
 set -ex
 # NOTE: Make sure this volume is mounted 'ro', otherwise Jenkins cannot clean up the
 # workspace due to file permission errors; the Z is used for SELinux workarounds
 # -e NODE_BUILD_VERSION can be used to specify a version other than 12
 docker run -i --name $CONTAINER_NAME \
   -v $PWD:/workspace:ro,Z \
-  -e QUAY_USER=$QUAY_USER \
-  -e QUAY_TOKEN=$QUAY_TOKEN \
   -e APP_DIR=$APP_DIR \
   -e IS_PR=$IS_PR \
   -e CI_ROOT=$CI_ROOT \
@@ -88,7 +80,6 @@ TEST_RESULT=$?
 
 if [ $TEST_RESULT -ne 0 ]; then
   echo "Test failure observed; aborting"
-  teardown_docker
   exit 1
 fi
 
@@ -98,54 +89,9 @@ docker cp $CONTAINER_NAME:/container_workspace/ $WORKSPACE/build
 cd $WORKSPACE/build/container_workspace/ && export APP_ROOT="$WORKSPACE/build/container_workspace/"
 
 
-
-# ---------------------------
-# Build and Publish to Quay
-# ---------------------------
-
-if [ $IS_PR = true ]; then
-  echo $'\n'>> $APP_ROOT/Dockerfile
-  # downstream developers may remove newline at end of dockerfile
-  # this will result in something like
-  #   CMD npm run start:containerLABEL quay.expires-after=3d
-  # instead of
-  #   CMD npm run start:container
-  #   LABEL quay.expires-after=3d
-  echo "LABEL quay.expires-after=3d" >> $APP_ROOT/Dockerfile # tag expires in 3 days
-else
-  echo "Publishing to Quay without expiration"
-fi
-
-# source <(curl -sSL $COMMON_BUILDER/src/quay_push.sh | bash -s)
-
-# IMAGE, IMAGE_TAG, and Tokens are exported from upstream pr_check.sh
-export LC_ALL=en_US.utf-8
-export LANG=en_US.utf-8
-export APP_ROOT=${APP_ROOT:-pwd}
-export WORKSPACE=${WORKSPACE:-$APP_ROOT}  # if running in jenkins, use the build's workspace
-# export IMAGE_TAG=$(git rev-parse --short=7 HEAD)
-export GIT_COMMIT=$(git rev-parse HEAD)
-
-
-if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
-    echo "QUAY_USER and QUAY_TOKEN must be set"
-    exit 1
-fi
-
-if [[ -z "$RH_REGISTRY_USER" || -z "$RH_REGISTRY_TOKEN" ]]; then
-    echo "RH_REGISTRY_USER and RH_REGISTRY_TOKEN must be set"
-    exit 1
-fi
-
 if [ $APP_NAME == "chrome" ] ; then
   get_chrome_config;
 fi
 
-DOCKER_CONF="$PWD/.docker"
-mkdir -p "$DOCKER_CONF"
-echo $QUAY_TOKEN | docker --config="$DOCKER_CONF" login -u="$QUAY_USER" --password-stdin quay.io
-echo $RH_REGISTRY_TOKEN | docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" --password-stdin registry.redhat.io
-docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" $APP_ROOT -f $APP_ROOT/Dockerfile
-docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
+docker build -t "${APP_NAME}:${IMAGE_TAG}" $APP_ROOT -f $APP_ROOT/Dockerfile
 
-teardown_docker
