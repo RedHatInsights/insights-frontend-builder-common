820fcc4c746c055e61a0e13be3781cd6e40b75bf
Adam Drew <addrew@redhat.com>
2023-03-17 16:15:31 -0400

    Bump frontend build container
    Gets us some caddy file fixes

--- src/frontend-build.sh
+++ src/frontend-build.sh
@@ -92,7 +92,7 @@ docker run -i --name $CONTAINER_NAME \
   -e CHROME_CONFIG_BRANCH \
   --add-host stage.foo.redhat.com:127.0.0.1 \
   --add-host prod.foo.redhat.com:127.0.0.1 \
-  quay.io/cloudservices/frontend-build-container:937f8d5
+  quay.io/cloudservices/frontend-build-container:8b281e3 
 TEST_RESULT=$?
 
 if [ $TEST_RESULT -ne 0 ]; then
