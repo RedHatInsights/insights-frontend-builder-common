03c006fb60331d3bf73f8c8c8aa380a93fdd81cb
Jan Richter <jrichter@redhat.com>
2023-09-12 10:50:48 +0200

    remove container if present instead of just running

--- src/frontend-build.sh
+++ src/frontend-build.sh
@@ -45,8 +45,8 @@ function teardown_docker() {
 
 trap "teardown_docker" EXIT SIGINT SIGTERM
 
-# Detect if the container is running
-if docker ps | grep $CONTAINER_NAME > /dev/null; then
+# Detect if the container exists
+if docker ps -a | grep $CONTAINER_NAME > /dev/null; then
   # Delete it
   # We do this because an aborted run could leave the container around
   teardown_docker
