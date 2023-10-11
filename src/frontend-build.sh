d0354a259c29030cffc141d072851ec0c28bbd1c
Jakub Rusz <jrusz@redhat.com>
2023-10-11 12:21:49 +0200

    Remove PR/MR prefix from image tag
    This is already being added in "bootstrap.sh" in the cicd-tools repo
    which is sourced before this script in "pr_check.sh".

--- src/frontend-build.sh
+++ src/frontend-build.sh
@@ -104,12 +104,10 @@ if echo $JOB_NAME | grep -w "pr-check" > /dev/null; then
   timestamp=$(date +%s)
 
   if [ ! -z "$ghprbPullId" ]; then
-    export IMAGE_TAG="pr-${ghprbPullId}-${IMAGE_TAG}"
     CONTAINER_NAME="${APP_NAME}-pr-check-${ghprbPullId}-${timestamp}"
   fi
 
   if [ ! -z "$gitlabMergeRequestIid" ]; then
-    export IMAGE_TAG="pr-${gitlabMergeRequestIid}-${IMAGE_TAG}"
     CONTAINER_NAME="${APP_NAME}-pr-check-${gitlabMergeRequestIid}-${timestamp}"
   fi
 
