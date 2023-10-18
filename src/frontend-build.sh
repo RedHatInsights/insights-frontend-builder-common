f08c65d7f05ae89528fa3c7a25493a57a01f4a3c
Adam Drew <addrew@redhat.com>
2023-10-18 08:43:37 -0400

    Update build image tage

--- src/frontend-build.sh
+++ src/frontend-build.sh
@@ -23,7 +23,7 @@ fi
 export IS_PR=false
 COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-frontend-builder-common/master
 EPOCH=$(date +%s)
-BUILD_IMAGE_TAG=c026352
+BUILD_IMAGE_TAG="6f41e6e"
 # Get current git branch
 # The current branch is going to be the GIT_BRANCH env var but with origin/ stripped off
 if [[ $GIT_BRANCH == origin/* ]]; then
