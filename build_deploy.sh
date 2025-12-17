c00c6c7a15ff28365f3d4c46014b97cb0a006d80
tahmidefaz <tahmidefaz@gmail.com>
2025-12-17 16:24:01 -0500

    removing unused jenkins and pr check and image build scripts for the removed app-interface job

--- build_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck source=/dev/null
-source <(curl -sSL https://raw.githubusercontent.com/RedHatInsights/cicd-tools/main/src/bootstrap.sh) image_builder
-
-export CICD_IMAGE_BUILDER_IMAGE_NAME='quay.io/cloudservices/releaser'
-export CICD_IMAGE_BUILDER_CONTAINERFILE_PATH='src/releaser.Dockerfile'
-
-cicd::image_builder::build_and_push
